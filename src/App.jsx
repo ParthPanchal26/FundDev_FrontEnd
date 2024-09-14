70e9ef352d64b0c3ae5d8c66222dd8ba3e2250a8
ParthPanchal26 <parthpanchal2625@gmail.com>
2024-09-14 23:25:38 +0530

    Created StarterPage component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Dev_Dashboard from './Components/Dev_Dashboard/Dev_Dashboard'
+import StarterPage from './Components/Starter_Page/StarterPage'
+
+const App = () => {
+  return (
+    // <Dev_Dashboard />
+    <StarterPage />
+  )
+}
+
+export default App
